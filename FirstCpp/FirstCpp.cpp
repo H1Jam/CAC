041f78fa28ba099f18e8ba560804027e775f794a
H1Jam <hosseinjamshidien@gmail.com>
2019-01-12 15:19:19 -0500

    Add project files.

---
+++ FirstCpp/FirstCpp.cpp
@@ -0,0 +1,15 @@
+// FirstCpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "23232";
+	getchar();
+    return 0;
+}
+
